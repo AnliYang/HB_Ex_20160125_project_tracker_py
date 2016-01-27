66f944f7a7e0caaff6621d905f574e7541cf1618
Hackbright Student <info@hackbrightacademy.com>
2016-01-26 18:02:52 -0800

    added get all grades for student and some excellent error handling

--- hackbright.py
+++ hackbright.py
@@ -5,6 +5,7 @@ projects, and the grades students receive in class projects.
 """
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+import sys
 
 db = SQLAlchemy()
 
@@ -88,6 +89,35 @@ def assign_grade(github, title, grade):
     print "Successfully added grade for %s on %s project." % (github, title)
 
 
+def get_all_grades_for_stud(github):
+    """Return all grades for a single student.
+
+    Prints one line per project, with that project's title and grade."""
+
+    QUERY = """
+        SELECT project_title, grade 
+        FROM grades
+        WHERE student_github = :github
+        """
+    db_cursor = db.session.execute (QUERY, {'github': github})
+    results = db_cursor.fetchall()
+    for stud in range(0,len(results)):
+        print results[stud][0], results[stud][1]
+
+
+def testy(first_name):
+    """dummy function for writing tests"""
+
+    QUERY = """
+        SELECT github 
+        FROM students
+        WHERE first_name = :first_name
+        """
+    db_cursor = db.session.execute (QUERY, {'first_name': first_name})
+    results = db_cursor.fetchone()
+    print results[0]
+
+
 def handle_input():
     """Main loop.
 
@@ -102,29 +132,42 @@ def handle_input():
         command = tokens[0]
         args = tokens[1:]
 
-        if command == "student":
-            github = args[0]
-            get_student_by_github(github)
+        try:
+            if command == "student":
+                github = args[0]
+                get_student_by_github(github)
 
-        elif command == "new_student":
-            first_name, last_name, github = args   # unpack!
-            make_new_student(first_name, last_name, github)
+            elif command == "new_student":
+                first_name, last_name, github = args   # unpack!
+                make_new_student(first_name, last_name, github)
 
-        elif command == "project":
-            title = args[0]
-            get_project_by_title(title)
+            elif command == "project":
+                title = args[0]
+                get_project_by_title(title)
 
-        elif command == "get_grade":
-            github, title = args
-            get_grade_by_github_title(github, title)
+            elif command == "get_grade":
+                github, title = args
+                get_grade_by_github_title(github, title)
 
-        elif command == "ass_grade":
-            github, title, grade = args
-            assign_grade(github, title, grade)
+            elif command == "ass_grade":
+                github, title, grade = args
+                assign_grade(github, title, grade)
 
-        else:
-            if command != "quit":
-                print "Invalid Entry. Try again."
+            elif command == "stud_grade":
+                github = args[0]
+                get_all_grades_for_stud(github)
+
+            elif command == "test":
+                first_name = args[0]
+                testy(first_name)
+
+            else:
+                if command != "quit":
+                    print "Invalid Entry. Try again."
+
+        except:
+            e = sys.exc_info()[0]
+            print " NEW Invalid Entry. Try again."
 
 
 if __name__ == "__main__":
